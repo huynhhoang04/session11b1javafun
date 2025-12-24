c509b8bff522ffe03d97f8eaba3ecfd2fd6413cc
huynhhoang04 <huynh696k@gmail.com>
2025-12-24 12:37:32 +0700

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,14 @@
+public abstract class Shape {
+    protected String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public abstract double getArea();
+    public abstract double getPerimeter();
+
+    public void displayInfo() {
+        System.out.println("Ten: " + name);
+    }
+}
